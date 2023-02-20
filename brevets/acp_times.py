2fabc70f85d151c8f26e351ab68161b46d937c61
vboxuser <simonz@uoregon.edu>
2023-02-19 16:11:02 -0800

    Revert "Added first test case"
    This reverts commit 425b4e41388bbf5d16d1d602c3688f44973c1b1c.

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -13,27 +13,6 @@ import arrow
 #  same arguments.
 #
 
-# figure out when checkpoint opens and closes
-# how long to wait until open or close is where it is using control_dist_km
-
-# brevet_start_time is an arrow object
-# hours and minutes should be integers (round minutes to nearest integer)
-
-
-
-
-
-# compute hrs and mins based on control_dist_km and shift brevet_start_time
-
-
-# first checkpoint at 0km always opens at brevet start time(not shifted)
-# and always closes after 1 hour(dont have to calculate, just shift by 1 hour)
-
-# brevet_dist_km. Figure out where first checkpoint and last checkpoint(s) is/are
-# how? if control_dist_km is >= brevet_dist_km, meaning if you have a 200km brevet, 210, up tp 240 (which is the max distance for a 200km brevet) (up to 20% higher), then its the last checkpoint
-# this is for open time
-# close time from the table in the link.
-
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -47,9 +26,9 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    hours, minutes =  # figure out how to calculate
-    return brevet_start_time.shift(hours=hours, minutes=minutes)
- 
+    return arrow.now()
+
+
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
     Args:
@@ -62,5 +41,4 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    hours, minutes =  # figure out how to calculate
-    return brevet_start_time.shift(hours=hours, minutes=minutes)
+    return arrow.now()
